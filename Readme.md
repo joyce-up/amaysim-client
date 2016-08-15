ff85b2ecd01a1a19e90797ef9be37ab5a05ce356
Joyce Avestro <joyce.avestro@gmail.com>
2016-08-15 09:20:42 +0800

    Modified to accept productId argument

--- Readme.md
+++ Readme.md
@@ -1,6 +1,11 @@
+# How to Build
+	- mvn clean install
+
+
 # How to Run
 
 Please run using Java (at least version 1.7):
 
-java -jar target/amaysim-client-0.0.1.jar
+	 - java -jar target/amaysim-client-0.0.1.jar - to list all the products
+	 - java -jar target/amaysim-client-0.0.1.jar <productId> - to list the product with id <productId>
 
--- src/main/java/com/amaysim_client/App.java
+++ src/main/java/com/amaysim_client/App.java
@@ -11,27 +11,45 @@ import org.springframework.web.client.RestTemplate;
 public class App implements CommandLineRunner {
 
 	private String productsUrl = "http://ec2-54-206-17-197.ap-southeast-2.compute.amazonaws.com/amaysim/products";
-	private String productByIdUrl = "http://ec2-54-206-17-197.ap-southeast-2.compute.amazonaws.com/amaysim/product/1";
+	private String productByIdUrl = "http://ec2-54-206-17-197.ap-southeast-2.compute.amazonaws.com/amaysim/product/";
 	
     public static void main(String args[]) {
-        SpringApplication.run(App.class);
+        SpringApplication.run(App.class, args);
     }
 
     public void run(String... args) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        List<Product> products = 
-        		(List<Product>) restTemplate.getForObject(productsUrl, List.class);
-        
-        System.out.println();
-        System.out.println(productsUrl + ": ");
-        System.out.println(products);
-        System.out.println();
         
         
-        Product product = restTemplate.getForObject(productByIdUrl, Product.class);
-        System.out.println();
-        System.out.println(productByIdUrl + ": ");
-        System.out.println(product);
-        System.out.println();
+        if (args.length == 0){
+	        List<Product> products = 
+	        		(List<Product>) restTemplate.getForObject(productsUrl, List.class);
+	        
+	        System.out.println();
+	        System.out.println(productsUrl + ": ");
+	        System.out.println(products);
+	        System.out.println();
+	        
+        } else if (args.length == 1){
+        	
+        	try{
+        	int id = Integer.parseInt(args[0]);
+	        Product product = restTemplate.getForObject(productByIdUrl + id, Product.class);
+	        System.out.println();
+	        System.out.println(productByIdUrl + "("+ id +"): ");
+	        System.out.println(product);
+	        System.out.println();
+        	} catch (java.lang.NumberFormatException e){
+        		printUsage();
+        	}
+        } else {
+        	printUsage();
+        }
+    }
+    
+    private void printUsage(){
+    	System.out.println("Usage: \n\t\"java -jar amaysim-client-0.0.1.jar\" to list all the products"
+    			+ "\n\t\"java -jar amaysim-client-0.0.1.jar\\ <productId>\" to list the product having the id <productId>");
+    	
     }
 }
